0ced8fa91f61b6308cbb96338b720d3d390bfe9e
mars <adenijifara@gmail.com>
2024-10-23 23:23:53 +0300

    Task 0 complete

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+This script writes strings to Redis
+'''
+
+import uuid
+import redis
+
+class Cache:
+    ''' A redis cache'''
+    def __init__(self) -> None:
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: str | int | float | bool) -> str:
+        '''Set a value in Redis'''
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
