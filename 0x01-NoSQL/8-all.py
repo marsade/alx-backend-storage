2eac403753cf9c569ed15ef296ef76d313052d7c
mars <adenijifara@gmail.com>
2024-10-23 22:17:50 +0300

    remove import

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 '''Lists all the documents in the MongoDB database'''
 
-# import pymongo
 
 def list_all(mongo_collection):
     '''Lists all documents in the specified collection'''
