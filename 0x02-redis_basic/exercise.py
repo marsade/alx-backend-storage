dd5d7dc9c809fdeb9c4211bd33391ca8fb987953
Mars Adeniji <adenijifara@gmail.com>
2024-12-07 22:01:32 +0200

    Annotate properly

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,7 @@ This script writes strings to Redis
 
 import uuid
 import redis
+from typing import Union
 
 class Cache:
     ''' A redis cache'''
@@ -13,7 +14,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: str | int | float | bool) -> str:
+    def store(self, data: Union[str, int, float, bool]) -> str:
         '''Set a value in Redis'''
         key = str(uuid.uuid4())
         self._redis.set(key, data)
